8aab960dc403b47283b873100d8f1495122d8abb
Bradon Thymes <bmt.wcpa@gmail.com>
2019-11-12 16:48:47 -0500

    Printed the prompt to the screen and initialized an array

--- project2.s
+++ project2.s
@@ -1,6 +1,13 @@
 .data
 inputMessage: .asciiz "Input a string: "
+#Initialize an array
+myArray: .space 11
 outputMessage: .asciiz "\nThe output is: "
 
 .text
     main:
+#Input Message
+        li $v0, 4
+        la $a0, inputMessage
+        syscall
+
