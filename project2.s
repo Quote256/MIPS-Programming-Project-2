89e8ddd8adc1ef3f401961c8e23ae3963995aea8
Bradon Thymes <bmt.wcpa@gmail.com>
2019-11-14 10:41:01 -0800

    Initialized array

--- project2.s
+++ project2.s
@@ -1,3 +1,7 @@
+#Input string of up to 1000 characters
+#Return unsigned integer corresponding with the base-N number represented by a string
+
+
 .data
 inputMessage: .asciiz "Input a string: "
 #Initialize an array
@@ -9,5 +13,14 @@ outputMessage: .asciiz "\nThe output is: "
 #Input Message
         li $v0, 4
         la $a0, inputMessage
+        syscall
+
+        #Initialize array
+        li $v0, 8 #opcode for  reading in a string
+        la $a0, myArray
+        li $a1, 11
+        syscall
+
+        li $v0, 10 #exits the program
         syscall
 
